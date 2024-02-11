5d4eae60db9d886dcbdaa83972cdfdf633a6950c
H. Chase Stevens <chase@chasestevens.com>
2024-02-11 00:37:33 -0500

    Update model.py

--- model.py
+++ model.py
@@ -109,9 +109,9 @@ class GPTConfig:
     vocab_size: int = 6064 # GPT-2 vocab_size of 50257, padded up to nearest multiple of 64 for efficiency
     n_layer: int = 12
     n_head: int = 64
-    n_embd: int = 512
+    n_embd: int = 256
     attention_proj_size: int = 64
-    mlp_intermediate_size: int = 4 * 768
+    mlp_intermediate_size: int = 4 * 256
     dropout: float = 0.0
     bias: bool = True # True: bias in Linears and LayerNorms, like GPT-2. False: a bit better and faster
 
