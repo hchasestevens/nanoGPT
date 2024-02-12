13bfb0af8e29ae6a1fbcfd22009b937c901627d0
H. Chase Stevens <chase@chasestevens.com>
2024-02-12 11:58:27 -0500

    Update model.py

--- model.py
+++ model.py
@@ -108,7 +108,7 @@ class GPTConfig:
     block_size: int = 512
     vocab_size: int = 6064 # GPT-2 vocab_size of 50257, padded up to nearest multiple of 64 for efficiency
     n_layer: int = 12
-    n_head: int = 64
+    n_head: int = 128
     n_embd: int = 128
     attention_proj_size: int = 64
     mlp_intermediate_size: int = 4 * 128
