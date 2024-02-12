021c3218ca3daefc2a9f91934b6a23800172c9ed
H. Chase Stevens <chase@chasestevens.com>
2024-02-12 17:02:52 -0500

    whoops (hadn't updated model)

--- config/train_enwik8_char_bloom2.py
+++ config/train_enwik8_char_bloom2.py
@@ -14,7 +14,7 @@ wandb_project = 'enwik8-char-bloom-2'
 wandb_run_name = 'mini-gpt'
 
 dataset = 'enwik8_char'
-gradient_accumulation_steps = 1
+gradient_accumulation_steps = 8
 batch_size = 64
 block_size = 512 # context of up to 512 previous characters
 
