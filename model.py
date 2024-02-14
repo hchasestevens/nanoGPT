a43d393d44e27e950b11ab35cfa78e56e00871a3
H. Chase Stevens <chase@chasestevens.com>
2024-02-14 13:35:30 -0500

    204.50M; 1.1116399060078046bpc! We did it reddit!

--- model.py
+++ model.py
@@ -28,7 +28,7 @@ class LayerNorm(nn.Module):
 
 class CausalSelfAttention(nn.Module):
 
-    def __init__(self, config):
+    def __init__(self, config): 
         super().__init__()
         assert config.n_embd % config.n_head == 0
         # key, query, value projections for all heads, but in a batch
