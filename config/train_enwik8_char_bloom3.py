0e29b49b10f7756079d161511c9231f1e94481ec
H. Chase Stevens <chase@chasestevens.com>
2024-02-22 10:04:32 -0500

    supposedly this previously did not oom??

--- config/train_enwik8_char_bloom3.py
+++ config/train_enwik8_char_bloom3.py
@@ -18,10 +18,10 @@ gradient_accumulation_steps = 1
 batch_size = 32
 block_size = 512 # context of up to 512 previous characters
 
-n_layer: int = 256
+n_layer: int = 512
 n_head: int = 4
 n_embd: int = 512
-attention_proj_size: int = 16
+attention_proj_size: int = 32
 causal_self_attn_size: int = 1
 mlp_intermediate_size: int = 4 * 512
 dropout: float = 0.01
