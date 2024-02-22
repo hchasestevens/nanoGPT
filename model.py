6b89faff6a1da72320ee62d3eb171fd80c8407f0
H. Chase Stevens <chase@chasestevens.com>
2024-02-22 12:46:30 -0500

    fix

--- model.py
+++ model.py
@@ -80,7 +80,6 @@ class KVCausalSelfAttention(nn.Module):
     def __init__(self, config):
         super().__init__()
         self.attention_proj_size = config.attention_proj_size
-        self.causal_self_attn_size = config.causal_self_attn_size
         self.c_attn = nn.Linear(config.n_embd, 2 * self.attention_proj_size, bias=config.bias)
         # output projection
         self.c_proj = nn.Linear(self.attention_proj_size, self.attn_head_size, bias=config.bias)
